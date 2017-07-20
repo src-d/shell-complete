b7fae4771a9ec238ab6aa8f1b780617b27656302
Vadim Markovtsev <vadim@sourced.tech>
2017-07-20 10:39:24 +0200

    Add CI and packaging

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shell-complete",
+    description="Part of source{d}'s stack for machine learning on source "
+                "code. Provides API and tools to train and use models for "
+                "predicting next shell commands.",
+    version="1.0.0",
+    license="Apache 2.0",
+    author="source{d}",
+    author_email="machine-learning@sourced.tech",
+    url="https://github.com/src-d/shell-complete",
+    download_url='https://github.com/src-d/shell-complete',
+    packages=find_packages(exclude=("shcomplete.tests",)),
+    keywords=["machine learning on source code", "shell", "bash"],
+    install_requires=["modelforge>=1.0.0", ],
+    package_data={"": ["LICENSE", "README.md"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries"
+    ]
+)
